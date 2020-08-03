afd2c9a7fb7b6560d57463eddef73602d0746ee2
Cameron <cameron@teamwebley.co.uk>
2020-08-03 11:39:07 +0100

    Removed cookie because it's potentially illegal whoops! Also added features to work better on mobile devices

--- blog/static/js/home.js
+++
@@ -1,9 +0,0 @@
-function checkCookie() {
-  if (document.cookie != "") {
-    document.getElementById("preloader").style.display = "none";
-  } else {
-    var date = new Date();
-    date.setTime(date.getTime() + 60*60*1000);
-    document.cookie = "expires=" + date.toGMTString();
-  }
-}
